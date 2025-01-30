9d9c9a200acfa0032b18bded8310b12ede37e2f8
amanchauhan-dev <amanchauhan0435@gmail.com>
2025-01-30 23:01:53 +0530

    Error Solving

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { useAppDispatch, useAppSelector } from "@/hooks/redux-store";
+import {  useAppSelector } from "@/hooks/redux-store";
 import { usePrefersTheme } from "react-haiku";
 
 const ThemeProvider = ({ children }: { children?: React.ReactNode }) => {
   let currentTheme = useAppSelector((s) => s.theme.theme);
   const theme = usePrefersTheme("light");
-  const dispatch = useAppDispatch();
 
   return (
     <body className={`${currentTheme == "system" ? theme : currentTheme}`}>

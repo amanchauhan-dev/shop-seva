9f10cbab90ae5f3996737aa9529ca8e642ed6c5a
amanchauhan-dev <amanchauhan0435@gmail.com>
2025-02-01 10:18:46 +0530

    Refactor layout component for improved readability and consistency

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,22 +1,18 @@
-'use client'
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar";
+"use client";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import Header from "./Header";
 import { AppSidebar } from "./sidebar";
 const Layout = ({ children }: { children: React.ReactNode }) => {
-
   return (
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <Header/>
-          <div className="flex-auto py-2 px-2 sm:px-4 lg:px-6 xl:px-8 ">
-            {children}
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <Header />
+        <div className="flex-auto py-2 px-2 sm:px-4 lg:px-6 xl:px-8 ">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
